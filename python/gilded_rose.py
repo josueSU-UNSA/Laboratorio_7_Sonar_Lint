fa5ea41cf489b51a904589bbed5e2c048a0386fb
josueSU-UNSA <jsumare@unsa.edu.pe>
2022-12-08 16:29:19 -0500

    no smells ,only to reduce cognitive complexity

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,31 +6,30 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        passes_backstage="Backstage passes to a TAFKAL80ETC concert"
+        sulfuras="Sulfuras, Hand of Ragnaros"
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != "Aged Brie" and item.name != passes_backstage:
+                if item.quality > 0 and item.name != sulfuras:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
+                    if item.name == passes_backstage:
+                        if item.sell_in < 11 and item.quality < 50:
                                 item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
+                        if item.sell_in < 6 and item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != sulfuras:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name != passes_backstage:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != sulfuras:
                                 item.quality = item.quality - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+import gilded_rose
 
-from gilded_rose import *
 
 if __name__ == "__main__":
+    passes_backstage="Backstage passes to a TAFKAL80ETC concert"
     print ("OMGHAI!")
     items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+            
+             gilded_rose.Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             gilded_rose.Item(name="Aged Brie", sell_in=2, quality=0),
+             gilded_rose.Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             gilded_rose.Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             gilded_rose.Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+             gilded_rose.Item(name=passes_backstage, sell_in=15, quality=20),
+             gilded_rose.Item(name=passes_backstage, sell_in=10, quality=49),
+             gilded_rose.Item(name=passes_backstage, sell_in=5, quality=49),
+             gilded_rose.Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
     days = 2
@@ -27,4 +29,4 @@ if __name__ == "__main__":
         for item in items:
             print(item)
         print("")
-        GildedRose(items).update_quality()
+        gilded_rose.GildedRose(items).update_quality()
